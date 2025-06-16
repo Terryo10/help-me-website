14321547e4b0d5698d29a46885ce9a77a7bf5348
pikigene01 <pikigene01@gmail.com>
2025-06-16 04:58:53 +0200

    update on app whole app page with livewire components

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,3 +1,286 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+  <header id="header">
+            <!-- start topbar -->
+            <div class="topbar">
+                <div class="container-fluid">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 col-12">
+                            <ul class="contact-info">
+                                <li>
+                                    <i class="ti-email"></i><span>support@aidus.com</span>
+                                </li>
+                                <li>
+                                    <a href="tel:+887869587496">
+                                        <i class="flaticon-phone"></i><span>+88 7869 5874 96</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <div class="contact-into">
+                                <div class="language-selector">
+                                    <select id="languageSelect">
+                                        <option value="en" data-icon="assets/images/language/1.svg">English</option>
+                                        <option value="sp" data-icon="assets/images/language/2.svg">Spanish</option>
+                                    </select>
+                                    <div class="custom-select-wrapper">
+                                        <div class="custom-select"></div>
+                                        <div class="custom-arrow"><i class="ti-angle-down"></i></div>
+                                    </div>
+                                    <div class="custom-options"></div>
+                                </div>
+                                <div class="pryment-selector">
+                                    <select>
+                                        <option value="usd">USD</option>
+                                        <option value="aud">AUD</option>
+                                    </select>
+                                </div>
+                                <ul class="social-media">
+                                    <li><a href="#"><i class="flaticon-facebook-app-symbol"></i></a></li>
+                                    <li><a href="#"><i class="flaticon-linkedin"></i></a></li>
+                                    <li><a href="#"><i class="flaticon-camera"></i></a></li>
+                                    <li><a href="#"><i class="flaticon-vimeo"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end topbar -->
+
+            <!-- start midbar -->
+            <div class="midbar">
+                <div class="container-fluid">
+                    <div class="row align-items-center">
+                        <div class="col-lg-3 col-12">
+                            <a href="index.html" class="logo-mid">
+                                <img src="assets/images/logo.svg" alt="">
+                            </a>
+                        </div>
+                        <div class="col-lg-9 col-12">
+                            <ul>
+                                <li>
+                                    <div class="icon">
+                                        <i class="flaticon-clock"></i>
+                                    </div>
+                                    <div class="text">
+                                        <h3>Opening Houres</h3>
+                                        <span>Mon _ Sat: 9.00 to 18.00</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fi flaticon-home-address"></i>
+                                    </div>
+                                    <div class="text">
+                                        <h3>Our Address</h3>
+                                        <span>Bowery St, New York, USA</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="flaticon-phone-1"></i>
+                                    </div>
+                                    <div class="text">
+                                        <h3>Contact Us</h3>
+                                        <span>(671) 555-0110</span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end midbar -->
+
+            <div class="wpo-site-header">
+                <nav class="navigation navbar navbar-expand-lg navbar-light">
+                    <div class="container-fluid">
+                        <div class="row align-items-center">
+                            <div class="col-lg-3 col-md-3 col-3 d-lg-none dl-block">
+                                <div class="mobail-menu">
+                                    <button type="button" class="navbar-toggler open-btn">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar first-angle"></span>
+                                        <span class="icon-bar middle-angle"></span>
+                                        <span class="icon-bar last-angle"></span>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="col-lg-0 col-md-6 col-6">
+                                <div class="navbar-header">
+                                    <a class="navbar-brand" href="index.html"><img src="assets/images/logo-2.svg"
+                                            alt=""></a>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 col-md-1 col-1">
+                                <div id="navbar" class="collapse navbar-collapse navigation-holder">
+                                    <button class="menu-close"><i class="ti-close"></i></button>
+                                    <ul class="nav navbar-nav mb-2 mb-lg-0">
+                                        <li class="menu-item-has-children">
+                                            <a class="active" href="#">Home</a>
+                                            <ul class="sub-menu">
+                                                <li><a class="active" href="index.html">Home style 1</a></li>
+                                                <li><a href="index-2.html">Home style 2</a></li>
+                                                <li><a href="index-3.html">Home style 3</a></li>
+                                                <li><a href="index-4.html">Home style 4</a></li>
+                                                <li><a href="index-5.html">Home style 5</a></li>
+                                                <li><a href="index-6.html">Home style 6</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item-has-children">
+                                            <a href="#">Pages</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="about.html">About</a></li>
+                                                <li><a href="donate.html">Donate</a></li>
+                                                <li class="menu-item-has-children">
+                                                    <a href="#">event</a>
+                                                    <ul class="sub-menu">
+                                                        <li><a href="event.html">event</a>
+                                                        </li>
+                                                        <li><a href="event-single.html">event Single</a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li class="menu-item-has-children">
+                                                    <a href="#">Creas</a>
+                                                    <ul class="sub-menu">
+                                                        <li><a href="causes.html">causes</a>
+                                                        </li>
+                                                        <li><a href="causes-single.html">causes Single</a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li class="menu-item-has-children">
+                                                    <a href="#">volunteer</a>
+                                                    <ul class="sub-menu">
+                                                        <li><a href="volunteer-1.html">volunteer</a>
+                                                        </li>
+                                                        <li><a href="volunteer-2.html">volunteer S2</a>
+                                                        </li>
+                                                        <li><a href="volunteer-single.html">volunteer single</a>
+                                                        </li>
+                                                        <li><a href="become-volunteer.html">become volunteer</a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="404.html">404 Error</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item-has-children">
+                                            <a href="#">services</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="service.html">services</a></li>
+                                                <li><a href="service-single.html">services Single</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item-has-children">
+                                            <a href="#">Portfolio</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="project.html">Portfolio</a></li>
+                                                <li><a href="project-single.html">Portfolio Single</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item-has-children">
+                                            <a href="#">Shop</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="shop.html">Shop</a></li>
+                                                <li><a href="shop-single.html">Shop Single</a></li>
+                                                <li><a href="cart.html">Cart</a></li>
+                                                <li><a href="checkout.html">Checkout</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item-has-children">
+                                            <a href="#">Blog</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="blog.html">Blog right sidebar</a></li>
+                                                <li><a href="blog-left-sidebar.html">Blog left sidebar</a></li>
+                                                <li><a href="blog-fullwidth.html">Blog fullwidth</a></li>
+                                                <li class="menu-item-has-children">
+                                                    <a href="#">Blog details</a>
+                                                    <ul class="sub-menu">
+                                                        <li><a href="blog-single.html">Blog details right sidebar</a>
+                                                        </li>
+                                                        <li><a href="blog-single-left-sidebar.html">Blog details left
+                                                                sidebar</a></li>
+                                                        <li><a href="blog-single-fullwidth.html">Blog details
+                                                                fullwidth</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="contact.html">contact</a></li>
+                                    </ul>
+                                </div>
+                                <!-- end of nav-collapse -->
+                            </div>
+                            <div class="col-lg-5 col-md-2 col-2">
+                                <div class="header-right">
+                                    <div class="header-search-form-wrapper">
+                                        <div class="cart-search-contact">
+                                            <button class="search-toggle-btn"><i
+                                                    class="fi flaticon-magnifying-glass"></i></button>
+                                            <div class="header-search-form">
+                                                <form>
+                                                    <div>
+                                                        <input type="text" class="form-control"
+                                                            placeholder="Search here...">
+                                                        <button type="submit"><i
+                                                                class="fi flaticon-magnifying-glass"></i></button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="mini-cart">
+                                        <button class="cart-toggle-btn"> <i class="fi flaticon-bag"></i> <span
+                                                class="cart-count">2</span></button>
+                                        <div class="mini-cart-content">
+                                            <button class="mini-cart-close"><i class="ti-close"></i></button>
+                                            <div class="mini-cart-items">
+                                                <div class="mini-cart-item clearfix">
+                                                    <div class="mini-cart-item-image">
+                                                        <a href="shop.html"><img
+                                                                src="assets/images/shop/mini-cart/img-1.jpg" alt></a>
+                                                    </div>
+                                                    <div class="mini-cart-item-des">
+                                                        <a href="shop.html">glass of juice</a>
+                                                        <span class="mini-cart-item-price">$20.15 x 1</span>
+                                                        <span class="mini-cart-item-quantity"><a href="#"><i
+                                                                    class="ti-close"></i></a></span>
+                                                    </div>
+                                                </div>
+                                                <div class="mini-cart-item clearfix">
+                                                    <div class="mini-cart-item-image">
+                                                        <a href="shop.html"><img
+                                                                src="assets/images/shop/mini-cart/img-2.jpg" alt></a>
+                                                    </div>
+                                                    <div class="mini-cart-item-des">
+                                                        <a href="shop.html">Water filters</a>
+                                                        <span class="mini-cart-item-price">$13.25 x 2</span>
+                                                        <span class="mini-cart-item-quantity"><a href="#"><i
+                                                                    class="ti-close"></i></a></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mini-cart-action clearfix">
+                                                <span class="mini-checkout-price">Subtotal: <span>$215.14</span></span>
+                                                <div class="mini-btn">
+                                                    <a href="checkout.html" class="view-cart-btn s1">Checkout</a>
+                                                    <a href="cart.html" class="view-cart-btn">View Cart</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="close-form">
+                                        <a class="theme-btn" href="donate.html">Donate now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end of container -->
+                </nav>
+            </div>
+
+        </header>
 </div>
--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,3 +1,5 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <livewire:header />
+
+    <livewire:footer />
 </div>
