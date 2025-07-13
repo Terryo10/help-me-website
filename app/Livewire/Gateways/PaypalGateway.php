31ce7eaa41f9eadab196a0d3e17f6e6054b64c25
pikigene01 <pikigene01@gmail.com>
2025-07-13 10:32:35 +0200

    update null values

--- app/Livewire/Gateways/PaypalGateway.php
+++ app/Livewire/Gateways/PaypalGateway.php
@@ -26,7 +26,7 @@ class PaypalGateway extends Component
         $this->donation_id = $donation_id;
         $donation = Donation::findOrFail($donation_id);
         $transaction_latest_donation = Transaction::where('donation_id',  $donation_id)->latest()->first();
-        $this->transaction_id = $transaction_latest_donation->id;
+        $this->transaction_id = $transaction_latest_donation->id ?? null;
         $this->amount = $donation->amount;
         $this->site_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
         $this->email = Auth::user()->email ?? $donation->email;
