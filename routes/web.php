114f02a67e5550dab846b306c379e5726dae7798
pikigene01 <pikigene01@gmail.com>
2025-07-15 23:52:08 +0200

    add

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\HomePage;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
-use App\Http\Services\EmailNotificationService;
+use App\Services\EmailNotificationService;
 
 Route::get('/', \App\Livewire\HomePage::class)->name('home');
 
